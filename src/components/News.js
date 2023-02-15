cbe03e5070fdcd78323377c7eeb48ca5745d2903
Abhishek Srivastav <abhisheksrivastavx@gmail.com>
2023-02-16 01:13:00 +0530

    complete project

--- src/components/News.js
+++ src/components/News.js
@@ -1,36 +1,92 @@
 import React, { Component } from 'react'
 import NewsItem from './NewsItem'
+import Spinner from './Spinner';
+import PropTypes from 'prop-types';
+import InfiniteScroll from 'react-infinite-scroll-component';
 
 export default class News extends Component {
-  constructor() {
-    super();
+  static defaultProps = {
+    country: 'in',
+    pageSize: 9,
+    category: 'general'
+  }
+
+  static propTypes = {
+    country: PropTypes.string,
+    pageSize: PropTypes.number,
+    category: PropTypes.string
+  }
+
+  capitalizeFirstLetter = (string) => {
+    return string.charAt(0).toUpperCase() + string.slice(1);
+  }
+
+  constructor(props) {
+    super(props);
     this.state = {
       articles: [],
-      loading: false
+      loading: true,
+      page: 1,
+      totalResults: 0
     }
+
+    document.title = `NewsHub - ${this.capitalizeFirstLetter(this.props.category)}`;
+  }
+
+  async updateNews() {
+    this.props.setProgress(10);
+    const url = `https://newsapi.org/v2/top-headlines?country=${this.props.country}&category=${this.props.category}&apiKey=${this.props.apiKey}&page=${this.state.page}&pageSize=${this.props.pageSize}`;
+    this.setState({ loading: true });
+    let data = await fetch(url);
+    this.props.setProgress(30);
+    let parsedData = await data.json();
+    this.props.setProgress(60);
+    this.setState({
+      articles: parsedData.articles,
+      totalResults: parsedData.totalResults,
+      loading: false
+    });
+    this.props.setProgress(100);
   }
 
   async componentDidMount() {
-    let url = "https://newsapi.org/v2/top-headlines?country=in&apiKey=2cddac1db72c48b09a0a3488862d729e";
+    this.updateNews();
+  }
+
+  fetchData = async () => {
+    const url = `https://newsapi.org/v2/top-headlines?country=${this.props.country}&category=${this.props.category}&apiKey=${this.props.apiKey}&page=${this.state.page + 1}&pageSize=${this.props.pageSize}`;
+    this.setState({ page: this.state.page + 1 });
     let data = await fetch(url);
     let parsedData = await data.json();
-    this.setState({articles: parsedData.articles});
+    this.setState({
+      articles: this.state.articles.concat(parsedData.articles),
+      totalResults: parsedData.totalResults
+    });
   }
 
   render() {
     return (
-      <div className='container my3'>
-        <h2>NewsHub - Top Headlines</h2>
+      <>
+        <h2 className='text-center' style={{ margin: "30px 0px", marginTop: "90px" }}>NewsHub - Top {this.capitalizeFirstLetter(this.props.category)} Headlines</h2>
+        {this.state.loading && <Spinner />}
 
-        <div className="row">
-          {this.state.articles.map((element) => {
-            return <div className="col-md-4" key={element.url}>
-              <NewsItem title={element.title} description={element.description} imageUrl={element.urlToImage} newsUrl={element.url} />
+        <InfiniteScroll
+          dataLength={this.state.articles.length}
+          next={this.fetchData}
+          hasMore={this.state.articles.length !== this.state.totalResults}
+          // loader={<Spinner />}
+        >
+          <div className="container">
+            <div className="row">
+              {this.state.articles.map((element) => {
+                return <div className="col-md-4" key={element.url}>
+                  <NewsItem title={element.title} description={element.description} imageUrl={element.urlToImage} newsUrl={element.url} author={element.author} date={element.publishedAt} />
+                </div>
+              })}
             </div>
-          })}
-        </div>
-
-      </div>
+          </div>
+        </InfiniteScroll>
+      </>
     )
   }
 }
--- src/components/NewsItem.js
+++ src/components/NewsItem.js
@@ -2,15 +2,16 @@ import React, { Component } from 'react'
 
 export default class NewsItem extends Component {
   render() {
-    let { title, description, imageUrl, newsUrl } = this.props;
+    let { title, description, imageUrl, newsUrl, author, date } = this.props;
     return (
       <div className='my-3'>
-        <div className="card" style={{ width: "18rem"}}>
+        <div className="card">
           <img src={imageUrl?imageUrl:"https://images.macrumors.com/t/GJLg8XqxYZ1E-aISKWKoQL2EfRM=/2250x/article-new/2021/06/iCloud-General-Feature.jpg"} className="card-img-top" alt="..." />
           <div className="card-body">
             <h5 className="card-title">{title}</h5>
             <p className="card-text">{description}</p>
-            <a href={newsUrl} target="_blank" rel="noreferrer"  className="btn btn-sm btn-primary">Go somewhere</a>
+            <p className='card-text'><small className='text-muted'>By {author? author : "unknown"} on {new Date(date).toGMTString()}</small></p>
+            <a href={newsUrl} target="_blank" rel="noreferrer"  className="btn btn-sm btn-dark">Go somewhere</a>
           </div>
         </div>
       </div>
